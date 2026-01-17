23c9e7531ad980a81dbf1702c10343f4d0e549b9
AfanasevStas <afanasevstas96@mail.ru>
2026-01-17 19:52:47 +0300

    Conveyer calculation DONE, need check.

--- Calc_2/Main.cpp
+++ Calc_2/Main.cpp
@@ -190,6 +190,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		static INT operation = 0;
 		static BOOL input = FALSE;
 		static BOOL input_operation = FALSE;
+		static BOOL executed = FALSE;
 
 
 		CHAR sz_display[MAX_PATH] = {};
@@ -199,6 +200,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
 		{
 			input_operation = FALSE;
+			executed = FALSE;
 			if (input == FALSE)ZeroMemory(sz_display, sizeof(sz_display));
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			if (sz_display[0] == '0' && sz_display[1] != '.')
@@ -239,6 +241,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				(a == DBL_MIN ? a : b) = atof(sz_display);
 				input = false;
 			}
+			if(!input_operation && !executed)SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
 			operation = LOWORD(wParam);
 			input_operation = TRUE;
 		}
@@ -257,6 +260,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDC_BUTTON_SLASH: a /= b; break;
 			}
 			input_operation = FALSE;
+			executed = TRUE;
 			if (a != DBL_MIN)
 			{
 				sprintf(sz_display, "%g", a);
