c7c5ba7bc5b0aed04082814c64a5fb6ce390d002
AfanasevStas <afanasevstas96@mail.ru>
2026-01-25 20:56:52 +0300

    Set ICON

--- Calc_2/Main.cpp
+++ Calc_2/Main.cpp
@@ -226,6 +226,8 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			NULL
 		);
 		SetSkinFromDLL(hwnd, "square_blue");
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 	}
 	break;
 	case WM_CTLCOLOREDIT:
